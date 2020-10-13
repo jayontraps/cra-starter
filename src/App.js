f5515502803e78aa65eced216225b33d6ab20dcb
Jason Righelato <jayontraps@gmail.com>
2020-10-13 12:13:35 +0100

    setup using styled-components and storybook

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle, darkTheme, defaultTheme } from "./utils";
+import { SignInModal } from "./components";
 
-function App() {
+const App = () => {
+  const [useDarkTheme, setUseDarkTheme] = useState(false);
+  const [showModal, setShowModal] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={useDarkTheme ? darkTheme : defaultTheme}>
+      <button onClick={() => setShowModal(!showModal)}>Toggle Modal</button>
+      <button
+        style={{ margin: "0 16px 24px", padding: "8px", background: "none" }}
+        onClick={() => setUseDarkTheme(true)}
+      >
+        Dark theme
+      </button>
+      <button
+        style={{ margin: "0 16px 24px", padding: "8px", background: "none" }}
+        onClick={() => setUseDarkTheme(false)}
+      >
+        Default theme
+      </button>
+      <div
+        style={{
+          background: useDarkTheme
+            ? defaultTheme.primaryColor
+            : darkTheme.primaryColor,
+          width: "100vw",
+          height: "90vh",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-around",
+        }}
+      >
+        <SignInModal {...{ showModal, setShowModal }} />
+      </div>
+      <GlobalStyle />
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
